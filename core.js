b4d93903ce9f6959c648bfa3a105ee7191b58944
wreckski <bcts1100@gmail.com>
2022-08-12 05:40:38 -0700

    began coding

--- core.js
+++ core.js
@@ -1,3 +1,18 @@
 // things needed
 //player variable
 //computer variable 
+
+let playerScore = 0;
+let computerScore = 0;
+
+//set buttons
+const buttons = document.querySelectorAll("input")
+
+//this sets the computer choices and makes a random selection.
+function computer(){
+    let choices = ["rock" , "paper", "scissors"]
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+//want to do
+// use a switch command to change images.
