c4baabe6da9e937ef09a1ee9e9c601999f99f0c2
wreckski <bcts1100@gmail.com>
2022-08-12 20:54:45 -0700

    updated js

--- core.js
+++ core.js
@@ -13,6 +13,29 @@ function computer(){
     let choices = ["rock" , "paper", "scissors"]
     return choices[Math.floor(Math.random() * choices.length)]
 }
-
+function play(playerSelection) {
+    let computerPlay = computerSelection
+    let result = ""
+    
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        playerScore ++;
+        result = ("you win!" +playerSelection + "beats" + computerSelection);
+    }
+    else if
+        (playerSelection === "scissors" && computerSelection === "paper"){
+            playerScore ++;
+        result = ("you win!" +playerSelection + "beats" + computerSelection);
+        }
+    else if
+        (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore ++;
+        result = ("you win!" +playerSelection + "beats" + computerSelection);
+        }
+}
 //want to do
 // use a switch command to change images.
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
